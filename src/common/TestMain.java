9a08d57aeaac4d2f06abd9da81eb189b9dcdb4db
minju08 <kim_minju08@eland.co.kr>
2017-03-08 15:28:17 +0900

    testMain

---
+++ src/common/TestMain.java
@@ -0,0 +1,14 @@
+package common;
+
+import firstSection.Example1;
+
+public class TestMain {
+	
+	public static void main(String[] args){
+	
+		Example1 ex1 = new Example1();
+		
+		ex1.selectSort();
+	}
+
+}
